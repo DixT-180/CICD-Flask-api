26b7e9114c59556c1d71ee636c9559b970f946c3
dikshyant thapa <dikshyant180@gmail.com>
2025-07-06 16:46:23 +0545

    code added

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Titanic Data Quality API is running"
+
+@app.route('/check', methods=['POST'])
+def check_data():
+    if 'file' not in request.files:
+        return jsonify({"error": "No file uploaded"}), 400
+
+    file = request.files['file']
+    try:
+        df = pd.read_csv(file)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+
+    report = {
+        "rows": len(df),
+        "columns": len(df.columns),
+        "missing_values": df.isnull().sum().to_dict(),
+        "duplicate_rows": int(df.duplicated().sum())
+    }
+
+    return jsonify(report)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
